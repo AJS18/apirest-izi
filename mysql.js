03b8f00d7c3579959a262b7c756136e5c4f0f6ea
Andre Soares <andre.soares@vaifacil.com>
2024-06-28 19:16:21 -0300

    adicionando nodemon

--- mysql.js
+++ mysql.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 var pool = mysql.createPool({
-    "user": "rccfb18w7cyjpk6z",
-    "password": "c75aes3qh0p1x7cu",
-    "database": "qjru2cfyxstn7f6c",
-    "host": "ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-    "port": 3306
+    "user": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT
 });
 
 module.exports.pool = pool;
