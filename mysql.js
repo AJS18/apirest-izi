0aeaaf1d334a4b44dfd563c75576abe718489b3d
Andre Soares <andre.soares@vaifacil.com>
2024-06-28 19:06:06 -0300

    adicionando banco jawsdb

--- mysql.js
+++ mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 var pool = mysql.createPool({
-    "user": "root",
-    "password": "123456",
-    "database": "prod",
-    "host": "localhost",
+    "user": "rccfb18w7cyjpk6z",
+    "password": "c75aes3qh0p1x7cu",
+    "database": "qjru2cfyxstn7f6c",
+    "host": "ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
     "port": 3306
 });
 
